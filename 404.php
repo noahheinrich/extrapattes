2c7ea6b69b82ecbbda0915fa84323fffe3b52336
Noah Heinrich <noahheinrich38@gmail.com>
2024-01-18 23:20:22 +0100

    Fin page 404 + fix panel

--- 404.php
+++ 404.php
@@ -8,17 +8,17 @@ $banner_image = get_field('banner_image');
 <section class="bannerF">
     <div class="container" style="background-image: url(<?php echo ($banner_image['sizes']['banner']); ?>);">
         <h1 class="h1-title">
-            <?php
-            echo $banner_title;
-            ?>
+            ERREUR 404
         </h1>
     </div>
 </section>
-<section class="content-404">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/404.svg" alt="" width="400px" height="400px" />
-    <p>Sinon vous pouvez jeter un œil à nos questions fréquentes !</p>
-    <a href="<?php echo get_page_link('14')?>#faq">Voir les questions fréquentes</a>
-</section>
+<div class="wrap">
+    <section class="content-404">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/404.svg" alt="" width="400px" height="400px" />
+        <p>Sinon vous pouvez jeter un œil à nos questions fréquentes !</p>
+        <a href="<?php echo get_page_link('14')?>#faq">Voir les questions fréquentes</a>
+    </section>
+</div>
 
 <?php
 get_footer();
